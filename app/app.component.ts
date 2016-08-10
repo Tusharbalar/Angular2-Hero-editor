42aaef4e354741ab86655f4755eb190851afa1b7
TusharBalar <balar.tushar1@gmail.com>
2016-08-10 22:48:37 +0530

    Create Hero class

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+            <h1>My First Angular 2 App</h1>
+            <h2>{{title}}</h2>
+            <h3>{{hero.name}} details!</h3>
+        `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = "Tour of Heroes";
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+export class Hero {
+  id: number;
+  name: string;
+}
