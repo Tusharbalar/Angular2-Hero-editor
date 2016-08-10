6504b5592cd7b989c599668fc9a16d57ffb6e1a4
TusharBalar <balar.tushar1@gmail.com>
2016-08-10 22:50:33 +0530

    Multi-line template strings

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-            <h1>My First Angular 2 App</h1>
-            <h2>{{title}}</h2>
-            <h3>{{hero.name}} details!</h3>
-        `
+    template:`
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div><label>name: </label>{{hero.name}}</div>
+    `
 })
 export class AppComponent {
     title = "Tour of Heroes";
