3d4b1d579e5ca9bca18a2d80a4a6cb91e79357e0
TusharBalar <balar.tushar1@gmail.com>
2016-08-10 22:52:28 +0530

    Editing Our Hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core';
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>{{hero.name}}</div>
-    `
+        <div>
+            <label>name: </label>
+            <input value="{{hero.name}}" placeholder="name">
+        </div>
+  `
+
 })
 export class AppComponent {
     title = "Tour of Heroes";
