e86cb34203b567e6acc438b7c600c45e80929a7a
TusharBalar <balar.tushar1@gmail.com>
2016-10-15 17:23:00 +0530

    Impliment promise based service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,7 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -28,6 +17,7 @@ const HEROES: Hero[] = [
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
+  providers: [HeroService],
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -79,9 +69,19 @@ const HEROES: Hero[] = [
   `]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   onSelect(hero: Hero) { this.selectedHero = hero; }
+  constructor(private heroService: HeroService) {
+    
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 }
