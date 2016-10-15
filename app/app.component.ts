25d6f2320ed943543678861bbe0ecf4205b23ef2
TusharBalar <balar.tushar1@gmail.com>
2016-10-15 22:35:59 +0530

    Impliment simple routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
-  `
+   <h1>{{title}}</h1>
+   <a routerLink="/heroes">Heroes</a>
+   <router-outlet></router-outlet>
+ `
 })
 
 export class AppComponent {
