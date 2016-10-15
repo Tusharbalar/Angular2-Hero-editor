e6038dd8d0752dad65bee4a3fe392069f638c523
TusharBalar <balar.tushar1@gmail.com>
2016-10-15 23:14:10 +0530

    Minor changes in routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-   <h1>{{title}}</h1>
-   <a routerLink="/heroes">Heroes</a>
-   <router-outlet></router-outlet>
- `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
 
 export class AppComponent {
--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule }   from '@angular/router';
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }     from './dashboard.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
