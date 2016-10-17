e065b0200bf7195a133ae10fbefb83b53a062286
TusharBalar <balar.tushar1@gmail.com>
2016-10-17 17:39:38 +0530

    Heroes details using router.navigate

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,6 +3,8 @@ import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
+import { Router } from '@angular/router';
+
 @Component({
   moduleId: module.id,
   selector: 'my-heroes',
@@ -11,17 +13,28 @@ import { HeroService } from './hero.service';
 })
 
 export class HeroesComponent implements OnInit {
+
   heroes: Hero[];
   selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
-  constructor(private heroService: HeroService) {
-    
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 
 }
