df25280a8f4eec734b5c24a972380f6de8208d1d
TusharBalar <balar.tushar1@gmail.com>
2016-10-17 15:47:26 +0530

    Impliment deshboard hero to details using router.navigate

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -13,7 +14,7 @@ export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private router: Router) {
 
   }
 
@@ -22,7 +23,8 @@ export class DashboardComponent implements OnInit {
   }
 
   gotoDetail(hero: Hero): void {
-    
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
   }
 
 }
