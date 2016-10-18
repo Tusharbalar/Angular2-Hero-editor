933b990029c6ab6c2c4aa6df3b0a3becc62294dc
TusharBalar <balar.tushar1@gmail.com>
2016-10-18 14:08:30 +0530

    Used styleUrls for external file

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -9,6 +9,7 @@ import { HeroService } from './hero.service';
   moduleId: module.id,
   selector: 'my-hero-detail',
   templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
