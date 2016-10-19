f18d759f095fbd5989383ad8295f6680b8314c8a
TusharBalar <balar.tushar1@gmail.com>
2016-10-19 13:29:20 +0530

    Edit heroes done

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -34,4 +34,8 @@ export class HeroDetailComponent implements OnInit {
       this.location.back();
     }
 
+    save(): void {
+      this.heroService.update(this.hero).then(() => this.goBack());
+    }
+
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 export class HeroService {
 
   private heroesUrl = 'app/heroes';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -26,6 +27,14 @@ export class HeroService {
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
     .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+    .toPromise()
+    .then(() => hero)
+    .catch(this.handleError);
   }
 
 }
